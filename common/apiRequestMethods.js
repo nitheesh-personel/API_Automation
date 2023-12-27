ff4a4d79ea9a1d42459a63aa590ab848a0332c28
nitheesh-personnel <n4nitheesh.syam@gmail.com>
2023-12-27 23:45:59 +0530

    second commit

---
+++ common/apiRequestMethods.js
@@ -0,0 +1,36 @@
+var supertest = require('supertest');
+const baseUrl = supertest('https://dummy.restapiexample.com/api/v1');
+const prodBaseUrl = supertest('https://api.instagram.com');
+
+
+exports.callApiGetRequest = async (apiEndPoint) => {
+    const response = await baseUrl.get(apiEndPoint);    
+    return response;
+}
+
+
+exports.callApiGetRequestWithParams = async (apiEndPoint, queryParamObj, region, language, userType, callingUser, token, userAgent) => {
+    const headerCopy = {
+      ...requestHeaders.commonCircleHeader,
+      'accept-region': region,
+      'accept-language': language,
+    }
+   
+    jwtToken = getBearerToken(userType, callingUser, token);
+  
+    joinString = "?"
+    if (apiEndPoint.includes("?")) {
+      joinString = "&"
+    }
+  
+    if (userAgent) {
+      headerCopy['user-agent'] = userAgent;
+    }
+  
+    const queryParam = joinString + queryString.stringify(queryParamObj);
+    const response = await baseUrl.get(apiEndPoint + queryParam)
+      .set(headerCopy)
+      .set("authorization", jwtToken);
+    return response;
+}
+  
--- test/sampleApi.test.js
+++ test/sampleApi.test.js
@@ -1,13 +1,11 @@
 const chai = require('chai');
 const expect = chai.expect;
-const supertest = require('supertest');
-
-const api = require("supertest")("https://dummy.restapiexample.com/api/v1");
+const appRequests = require("../common/apiRequestMethods");
 
 describe('API Tests', () => {
   it('should return status 200 for a GET request', async () => {
-    const response = await api.get('/employees');
-    expect(response.status).to.equal(200);
+    const response = await appRequests.callApiGetRequest('/employees');
+    expect(response.status).to.equal(200, "Wrong response code");
   });
 
   // Add more test cases as needed
